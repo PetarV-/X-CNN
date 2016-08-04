f7b39d422ad2da2d83caf3884926139d5c2c9bd3
Petar Veličković <pv273@cam.ac.uk>
2016-08-04 05:11:25 +0100

    Added the FitNet4

---
+++ models/cifar10_fitnet.py
@@ -0,0 +1,187 @@
+'''
+This will attempt to reimplement the FitNet4 network
+as described by Romero et al. (2014)
+'''
+
+from __future__ import print_function
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Model
+from keras.layers import Input, Dense, Activation, Flatten, Dropout, merge, MaxoutDense
+from keras.layers import Convolution2D, MaxPooling2D, ZeroPadding2D
+from keras.optimizers import Adam
+from keras.utils.visualize_util import plot
+from utils.preprocess import get_cifar
+
+batch_size = 128
+nb_classes = 10
+nb_epoch = 474
+data_augmentation = True
+
+# plot the model?
+plot_model = True
+show_shapes = True
+plot_file = 'cifar10_maxout.png'
+
+# show the summary?
+show_summary = True
+
+# the data, shuffled and split between train and test sets
+(X_train, Y_train), (X_test, Y_test) = get_cifar(p=1.0, append_test=False, use_c10=True)
+print('X_train shape:', X_train.shape)
+print(X_train.shape[0], 'train samples')
+print(X_test.shape[0], 'test samples')
+
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+
+datagen = ImageDataGenerator(
+        samplewise_center=True,  # set each sample mean to 0
+        samplewise_std_normalization=True,  # divide each input by its std
+        zca_whitening=True)  # apply ZCA whitening
+
+datagen.fit(X_train)
+
+for X_b, Y_b in datagen.flow(X_train, Y_train):
+    X_train = X_b
+    Y_train = Y_b
+    break
+
+for X_b, Y_b in datagen.flow(X_test, Y_test):
+    X_test = X_b
+    Y_test = Y_b
+    break
+
+inputYUV = Input(shape=(3, 32, 32))
+
+# input_drop = Dropout(0.2)(inputYUV) (?)
+
+# This is a single convolutional maxout layer.
+h0_conv_a = Convolution2D(32, 3, 3, border_mode='same')(inputYUV)
+h0_conv_b = Convolution2D(32, 3, 3, border_mode='same')(inputYUV)
+h0_conv = merge([h0_conv_a, h0_conv_b], mode='max', concat_axis=1)
+
+h1_conv_a = Convolution2D(32, 3, 3, border_mode='same')(h0_conv)
+h1_conv_b = Convolution2D(32, 3, 3, border_mode='same')(h0_conv)
+h1_conv = merge([h1_conv_a, h1_conv_b], mode='max', concat_axis=1)
+
+h2_conv_a = Convolution2D(32, 3, 3, border_mode='same')(h1_conv)
+h2_conv_b = Convolution2D(32, 3, 3, border_mode='same')(h1_conv)
+h2_conv = merge([h2_conv_a, h2_conv_b], mode='max', concat_axis=1)
+
+h3_conv_a = Convolution2D(48, 3, 3, border_mode='same')(h2_conv)
+h3_conv_b = Convolution2D(48, 3, 3, border_mode='same')(h2_conv)
+h3_conv = merge([h3_conv_a, h3_conv_b], mode='max', concat_axis=1)
+
+h4_conv_a = Convolution2D(48, 3, 3, border_mode='same')(h3_conv)
+h4_conv_b = Convolution2D(48, 3, 3, border_mode='same')(h3_conv)
+h4_conv = merge([h4_conv_a, h4_conv_b], mode='max', concat_axis=1)
+
+h4_pool = MaxPooling2D(pool_size=(2, 2))(h4_conv)
+h4_drop = Dropout(0.2)(h4_pool)
+
+h5_conv_a = Convolution2D(80, 3, 3, border_mode='same')(h4_drop)
+h5_conv_b = Convolution2D(80, 3, 3, border_mode='same')(h4_drop)
+h5_conv = merge([h5_conv_a, h5_conv_b], mode='max', concat_axis=1)
+
+h6_conv_a = Convolution2D(80, 3, 3, border_mode='same')(h5_conv)
+h6_conv_b = Convolution2D(80, 3, 3, border_mode='same')(h5_conv)
+h6_conv = merge([h6_conv_a, h6_conv_b], mode='max', concat_axis=1)
+
+h7_conv_a = Convolution2D(80, 3, 3, border_mode='same')(h6_conv)
+h7_conv_b = Convolution2D(80, 3, 3, border_mode='same')(h6_conv)
+h7_conv = merge([h7_conv_a, h7_conv_b], mode='max', concat_axis=1)
+
+h8_conv_a = Convolution2D(80, 3, 3, border_mode='same')(h7_conv)
+h8_conv_b = Convolution2D(80, 3, 3, border_mode='same')(h7_conv)
+h8_conv = merge([h8_conv_a, h8_conv_b], mode='max', concat_axis=1)
+
+h9_conv_a = Convolution2D(80, 3, 3, border_mode='same')(h8_conv)
+h9_conv_b = Convolution2D(80, 3, 3, border_mode='same')(h8_conv)
+h9_conv = merge([h9_conv_a, h9_conv_b], mode='max', concat_axis=1)
+
+h10_conv_a = Convolution2D(80, 3, 3, border_mode='same')(h9_conv)
+h10_conv_b = Convolution2D(80, 3, 3, border_mode='same')(h9_conv)
+h10_conv = merge([h10_conv_a, h10_conv_b], mode='max', concat_axis=1)
+
+h10_pool = MaxPooling2D(pool_size=(2, 2))(h10_conv)
+h10_drop = Dropout(0.2)(h10_pool)
+
+h11_conv_a = Convolution2D(128, 3, 3, border_mode='same')(h10_drop)
+h11_conv_b = Convolution2D(128, 3, 3, border_mode='same')(h10_drop)
+h11_conv = merge([h11_conv_a, h11_conv_b], mode='max', concat_axis=1)
+
+h12_conv_a = Convolution2D(128, 3, 3, border_mode='same')(h11_conv)
+h12_conv_b = Convolution2D(128, 3, 3, border_mode='same')(h11_conv)
+h12_conv = merge([h12_conv_a, h12_conv_b], mode='max', concat_axis=1)
+
+h13_conv_a = Convolution2D(128, 3, 3, border_mode='same')(h12_conv)
+h13_conv_b = Convolution2D(128, 3, 3, border_mode='same')(h12_conv)
+h13_conv = merge([h13_conv_a, h13_conv_b], mode='max', concat_axis=1)
+
+h14_conv_a = Convolution2D(128, 3, 3, border_mode='same')(h13_conv)
+h14_conv_b = Convolution2D(128, 3, 3, border_mode='same')(h13_conv)
+h14_conv = merge([h14_conv_a, h14_conv_b], mode='max', concat_axis=1)
+
+h15_conv_a = Convolution2D(128, 3, 3, border_mode='same')(h14_conv)
+h15_conv_b = Convolution2D(128, 3, 3, border_mode='same')(h14_conv)
+h15_conv = merge([h15_conv_a, h15_conv_b], mode='max', concat_axis=1)
+
+h16_conv_a = Convolution2D(128, 3, 3, border_mode='same')(h15_conv)
+h16_conv_b = Convolution2D(128, 3, 3, border_mode='same')(h15_conv)
+h16_conv = merge([h16_conv_a, h16_conv_b], mode='max', concat_axis=1)
+
+h16_pool = MaxPooling2D(pool_size=(8, 8))(h16_conv)
+h16_drop = Dropout(0.2)(h16_pool)
+
+h16 = Flatten()(h16_drop)
+h17 = MaxoutDense(500, nb_features=5)(h16)
+h17_drop = Dropout(0.2)(h17)
+out = Dense(10, activation='softmax')(h17)
+
+model = Model(input=inputYUV, output=out)
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+if show_summary:
+    print(model.summary())
+
+if plot_model:
+    plot(model, show_shapes=show_shapes, to_file=plot_file)
+
+if not data_augmentation:
+    print('Not using data augmentation.')
+    model.fit(X_train, Y_train,
+              batch_size=batch_size,
+              nb_epoch=nb_epoch,
+              validation_data=(X_test, Y_test),
+              shuffle=True,
+              verbose=2)
+else:
+    print('Using real-time data augmentation.')
+
+    # this will do preprocessing and realtime data augmentation
+    datagen = ImageDataGenerator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
+        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip=True,  # randomly flip images
+        vertical_flip=False)  # randomly flip images
+
+    # compute quantities required for featurewise normalization
+    # (std, mean, and principal components if ZCA whitening is applied)
+    datagen.fit(X_train)
+
+    # fit the model on the batches generated by datagen.flow()
+    model.fit_generator(datagen.flow(X_train, Y_train,
+                        batch_size=batch_size),
+                        samples_per_epoch=X_train.shape[0],
+                        nb_epoch=nb_epoch,
+                        validation_data=(X_test, Y_test),
+                        verbose=2)
